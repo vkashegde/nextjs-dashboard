d29cf0646c1c92e9d8a27f58a20a67dcff331396
vkashegde <vikas@kofluence.com>
2025-02-19 18:14:20 +0530

    chapters 1,2,3,4,5

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -23,7 +23,7 @@ export type Invoice = {
   date: string;
   // In TypeScript, this is called a string union type.
   // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
-  status: 'pending' | 'paid';
+  status: "pending" | "paid";
 };
 
 export type Revenue = {
@@ -40,7 +40,7 @@ export type LatestInvoice = {
 };
 
 // The database returns a number for amount, but we later format it to a string with the formatCurrency function
-export type LatestInvoiceRaw = Omit<LatestInvoice, 'amount'> & {
+export type LatestInvoiceRaw = Omit<LatestInvoice, "amount"> & {
   amount: number;
 };
 
@@ -52,7 +52,7 @@ export type InvoicesTable = {
   image_url: string;
   date: string;
   amount: number;
-  status: 'pending' | 'paid';
+  status: "pending" | "paid";
 };
 
 export type CustomersTableType = {
@@ -75,6 +75,13 @@ export type FormattedCustomersTable = {
   total_paid: string;
 };
 
+export type YoutubeUserType = {
+  follower_count: number;
+  subscriber_count: number;
+  username: string;
+  profile_picture: String;
+  status: "active" | "pending";
+};
 export type CustomerField = {
   id: string;
   name: string;
@@ -84,5 +91,5 @@ export type InvoiceForm = {
   id: string;
   customer_id: string;
   amount: number;
-  status: 'pending' | 'paid';
+  status: "pending" | "paid";
 };
